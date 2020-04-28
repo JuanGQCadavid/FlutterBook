b8857ded75bcb6157e9842ba2a2e3306f8f6c131
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-04-28 06:36:04 -0500

    Before Buttons list

--- chapter3/counter_app/lib/main.dart
+++ chapter3/counter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/physics.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,27 +41,95 @@ class SubmiteButton extends StatelessWidget{
   }
 }
 
-class Cat {
-  String name;
-  String _color;
-  
-  void meow() => print("Meow");
-  void _purr() => print("Prrrr");
+class FancyButton extends StatefulWidget{
+  @override
+  final VoidCallback onPressed;
+  final Widget child;
+  const FancyButton({Key key, this.onPressed,this.child}) : super(key: key);
+  State<StatefulWidget> createState()  => _FancyButtonState();
+}
+class _FancyButtonState extends State<FancyButton>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        color: _getColors(),
+        child: widget.child,
+        onPressed: widget.onPressed
+      ),
+    );
+  }
+  Color _getColors(){
+    return _buttonColors.putIfAbsent(this, () => colors[next(0, 5)]);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Map<_FancyButtonState, Color> _buttonColors = {};
+final _random = Random();
+int next(int min, int max) => min + _random.nextInt(max - min);
+List<Color> colors = [
+  Colors.blue,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.amber,
+  Colors.lightBlue,
+];
 
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter;
+  bool _reversed = false;
+  List<UniqueKey> _buttonKeys = [UniqueKey(),UniqueKey()];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this._counter = 0;
+  }
   void _incrementCounter() {
-    Cat nora = Cat();
-    nora._color = "Orange";
     setState(() {
       _counter++;
     });
   }
 
+  void _resetCounter(){
+    setState(() => _counter = 0);
+    _swap();
+  }
+
+  void _swap() {
+    setState(() {
+      _reversed = !_reversed;
+    });
+  }
+  void _decrementCounter(){
+    setState(() => _counter-- );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final incrementButton = FancyButton(
+      child: Text(
+        "Increment",
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: _incrementCounter,
+    );
+
+    final decrementButton = FancyButton(
+      child: Text(
+        "Decrement",
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+
+    List<Widget> _buttons = <Widget>[incrementButton,decrementButton];
+
+    if(_reversed){
+      _buttons = _buttons.reversed.toList();
+    }
+
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -93,29 +163,56 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              child: Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text("Padded text")
-              )
-            ),
-            Container(
-              child: SubmiteButton.named("HIja del diablo"),
+              margin: EdgeInsets.only(bottom: 100.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.blue.withOpacity(0.25),
+                borderRadius: BorderRadius.circular(4.0)
+              ),
+
+              child: Image.asset(
+                'assets/images/flutter_logo_1080.png',
+                width: 100.0,
+              ),
             ),
 
             Text(
-              'Seleccionar tinto',
+              'You have pushed the button this many thimes: ',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text(
+                      "Decrement",
+                      style: TextStyle(color: Colors.white)
+                  ),
+                  onPressed: _decrementCounter,
+                  color: Colors.red
+                ),
+                RaisedButton(
+                  color: Colors.green,
+                  child: Text(
+                    "Increment",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: _incrementCounter,
+
+                ),
+              ],
+            )
+
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _resetCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.launch),
+        child: Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
